861b044f2932eebbbef520665e2423f9a23cae22
Joseph Monaco <jmonaco@jhu.edu>
2019-06-27 13:33:59 -0400

    Refactor project constants & numpy imports to toolbox

---
+++ toolbox/constants.py
@@ -0,0 +1,7 @@
+"""
+Globstar constant values for interactive work or simulation code.
+"""
+
+TWOPI    = twopi    = 2*pi
+ONEMEPS  = onemeps  = nextafter(1, 0)
+ZEROPEPS = zeropeps = nextafter(0, 1)
