d7b7d0979ec0a43e5664eb3f775d423f4f9d9768
Joseph Monaco <jmonaco@jhu.edu>
2019-07-21 08:02:54 -0400

    [toolbox] Add simulation context to shared state

--- tenko/mixins/simulator.py
+++ tenko/mixins/simulator.py
@@ -9,10 +9,17 @@ class SimulatorMixin(object):
         """
         Set simulation parameters in global scope and shared state.
         """
-        self._get_global_scope().update(params)
-        self._get_state().update(params)
+        Global = self._get_global_scope()
+        State = self._get_state()
+
+        Global.update(params)
+        State.update(params)
+
         self.write_json(params, 'simulation')
 
         self.out('Simulation parameters:')
         for name, value in params.items():
             self.out(f'- {name} = {value}', hideprefix=True)
+
+        # Put the context itself into shared state for project-wide access
+        State.update(context=self)
